0845394b507c248dd7d368138700f86ccdc0c1eb
Utaewook <ddd050427@gmail.com>
2022-04-08 16:28:41 +0900

    2022.04.08. 16:28 코딩테스트 로또 최고순위와 최저순위

--- python_workspace/coding_test/programmers/Lotto_Min_Max.py
+++ python_workspace/coding_test/programmers/Lotto_Min_Max.py
@@ -1,20 +1,47 @@
 def solution(lottos, win_nums):
-    answer = [0,0]
+    answer = [0, 0]
     zeros = len([x for x in lottos if x == 0])
     inter = len(set(lottos).intersection(set(win_nums)))
-    inter_min,inter_max = inter,inter+zeros
-    if zeros == 6:
-        answer[0] = 1
-        answer[1] = 6
-    elif zeros == 0:
-        answer[0] = 7 - inter_max
-        answer[1] = 7 - inter_max
-    elif inter_min < 2:
-        answer[0] = 6
-    elif inter_max < 2:
-        answer[1] = 6
+    inter_min, inter_max = inter, inter + zeros
+    if zeros == 0:
+        if inter == 0:
+            answer = [6, 6]
+        elif inter == 6:
+            answer = [1, 1]
+        else:
+            answer = [7 - inter_max, 7 - inter_min]
+    elif zeros == 6:
+        answer = [1, 6]
     else:
-        answer[0] = 7 - inter_max
-        answer[1] = 7 - inter_min
-        
+        if inter == 0:
+            answer = [7 - zeros, 6]
+        else:
+            answer = [7 - inter_max, 7 - inter_min]
+
     return answer
+
+
+# 다른 사람의 풀이
+
+# def solution(lottos, win_nums):
+#
+#     rank=[6,6,5,4,3,2,1]
+#
+#     cnt_0 = lottos.count(0)
+#     ans = 0
+#     for x in win_nums:
+#         if x in lottos:
+#             ans += 1
+#     return rank[cnt_0 + ans],rank[ans]
+
+# def solution(lottos, win_nums):
+#     rank = {
+#         0: 6,
+#         1: 6,
+#         2: 5,
+#         3: 4,
+#         4: 3,
+#         5: 2,
+#         6: 1
+#     }
+#     return [rank[len(set(lottos) & set(win_nums)) + lottos.count(0)], rank[len(set(lottos) & set(win_nums))]]
