fb032b149f04e54519b5e8abf3076fd854390d5e
Utaewook <ddd050427@gmail.com>
2022-06-03 20:21:10 +0900

    2022-06-03 커밋

--- python_workspace/baek_joon/DP/ZeroOneTile.py
+++ python_workspace/baek_joon/DP/ZeroOneTile.py
@@ -1,2 +1,14 @@
+
 N = int(input())
 
+count = 0
+dic = {}
+
+dic[0] = 0
+dic[1] = 1
+dic[2] = 2
+
+for i in range(3, N+1):
+    dic[i] = (dic[i-1]+dic[i-2]) % 15746
+
+print(dic[N])
--- python_workspace/coding_test/baek_joon/samsung/Resignment.py
+++ python_workspace/coding_test/baek_joon/samsung/Resignment.py
@@ -32,40 +32,17 @@
 # print(ss)
 
 N = int(input())
-schedule = []
+schedule = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
-for n in range(N):
-    schedule.append(tuple(map(int, input().split())))
+counsel_queue = [0]*30
 
-counsel_queue = []
-
-def func(day):
-    print(counsel_queue)
-    if day < 0:
-        return sum([schedule[x][1] for x in counsel_queue])
-    elif day + schedule[day][0] > N:
-        return func(day-1)
-    elif len(counsel_queue) == 0 or schedule[day][0] == 1:
-        counsel_queue.append(day)
-        return func(day-1)
+for i in range(N):
+    a = max(schedule[i][1],counsel_queue[i+schedule[i][0]+1])
+    for t in range(i+schedule[i][0]+1,N+2):
+        counsel_queue[t] = max(a,counsel_queue[t])
     else:
-        if day+schedule[day][0] < counsel_queue[0]:
-            if sum([schedule[x][1] if day <= x < day+schedule[day][0] else 0 for x in counsel_queue]) < schedule[day][1]:
-                for idx in counsel_queue:
-                    if idx < day+schedule[day][0]:
-                        counsel_queue.remove(idx)
-                counsel_queue.append(day)
-        else:
-            if sum([schedule[x][1] if day <= x < day+schedule[day][0] else 0 for x in counsel_queue]) < schedule[day][1]:
-                for idx in counsel_queue:
-                    if idx < day+schedule[day][0]:
-                        counsel_queue.remove(idx)
-                counsel_queue.append(day)
-            elif sum([schedule[x][1] for x in counsel_queue]) < schedule[day][1]:
-                counsel_queue.clear()
-                counsel_queue.append(day)
-            else:
-                counsel_queue.append(day)
-        return func(day-1)
+        a = max(max(counsel_queue[:i+2])+schedule[i][1],counsel_queue[i+schedule[i][0]+1])
+        for t in range(i+schedule[i][0]+1,N+2):
+            counsel_queue[t] = max(a,counsel_queue[t])
 
-print(func(N-1))
+print(max(counsel_queue[:N+2]))
