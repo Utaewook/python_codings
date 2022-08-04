bf29c187b494e3860fd943a6bea278351b9a7bf1
유태욱 <ddd050427@gmail.com>
2022-08-04 15:47:14 +0900

    2022.08.04 Dp study

--- python_workspace/baek_joon/DP/RGB_Street.py
+++ python_workspace/baek_joon/DP/RGB_Street.py
@@ -1,9 +1,43 @@
 N = int(input())
 
-R,G,B = 0,1,2
-cost = []
-
+R, G, B = 0, 1, 2 # color index
+input_costs = []
 for _ in range(N):
-    cost.append(map(int,input().split()))
+    input_costs.append(list(map(int, input().split())))
 
-sum = 0
+s = 0
+mem = list()
+now = -1
+for i in range(N):
+    tempArray = list()
+    if min(input_costs[i]) == input_costs[i][R]:
+        if now == R and min(input_costs[i]) == input_costs[i][R]:
+            tempArray = [input_costs[i][G], input_costs[i][B]]
+            if min(tempArray) == tempArray[0]:
+                now = G
+            else:
+                now = B
+        else:
+            now = R
+    elif min(input_costs[i]) == input_costs[i][G]:
+        if now == G and min(input_costs[i]) == input_costs[i][G]:
+            tempArray = [input_costs[i][R], input_costs[i][B]]
+            if min(tempArray) == tempArray[0]:
+                now = R
+            else:
+                now = B
+        else:
+            now = G
+    elif min(input_costs[i]) == input_costs[i][B]:
+        if now == B and min(input_costs[i]) == input_costs[i][B]:
+            tempArray = [input_costs[i][R], input_costs[i][G]]
+            if min(tempArray) == tempArray[0]:
+                now = R
+            else:
+                now = G
+        else:
+            now = B
+    mem.append(now)
+
+print(sum([input_costs[idx][mem[idx]] for idx in range(N)]))
+# 최소값 찾지 못함 - dp 활용 할 것
