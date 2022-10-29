7255431ea8b9c57ad82ffc79593acca4da76ad11
유태욱 <ddd050427@gmail.com>
2022-10-29 17:16:54 +0900

    2022.10.29 temp

---
+++ python_workspace/coding_test/baek_joon/samsung/supply_route.py
@@ -0,0 +1,13 @@
+test_case = int(input())
+
+
+def dfs(r,c,s):
+
+
+def bfs(r,c,s):
+
+
+for test in range(test_case):
+    n = int(input())
+    MAP = [[*map(int,input().split())] for _ in range(n)]
+    visited = [[[False]*n] for _ in range(n)]
